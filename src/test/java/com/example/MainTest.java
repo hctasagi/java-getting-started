6a4303aa65218dddda9dc0a32265549237c1afaa
agarihiga <agarihiga@humancrest.co.jp>
2019-11-07 12:14:49 +0900

    add selenium

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -2,6 +2,8 @@ package com.example;
 
 
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class MainTest extends Main {
@@ -9,6 +11,14 @@ public class MainTest extends Main {
 	@Test
 	public void test1() {
 
+		System.setProperty("webdriver.chrome.driver", "/app/.chromedriver/bin/chromedriver");
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu");
+
+		ChromeDriver driver = new ChromeDriver(options);
+		driver.get("https://www.google.com");
+		driver.close();
 
 		System.out.println("aa");
 
